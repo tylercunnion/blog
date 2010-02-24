8c2b76c4858c79ec86dfc4d02136cdb15b503e79
Tyler Cunnion <tyler.cunnion@gmail.com>
2010-02-24 11:40:55 -0500

    Added post controller w/ boilerplate REST actions, views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+	def index
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def create
+	end
+
+	def show
+	end
+
+	def update
+	end
+
+	def delete
+	end
+end
