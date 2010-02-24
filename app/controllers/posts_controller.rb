7d8065360135ec5dd1923647ad115188d47dc5c9
Tyler Cunnion <tyler.cunnion@gmail.com>
2010-02-24 15:50:58 -0500

    Fleshing out posts views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,42 @@
 class PostsController < ApplicationController
+
+	before_filter :require_user, :only => [:edit, :update, :new, :create, :delete]
+
 	def index
-		@posts = Post.all
+		@posts = Post.all(:include => :author)
 	end
 
 	def new
+		@post = Post.new
+	end
+
+	def create 
+		@post = Post.new(params[:post])
+		@post.author = @current_user
+		if @post.save
+			flash[:notice] = "Post saved."
+			redirect_to post_url(@post)
+		else
+			render :action => :new
+		end
 	end
 
 	def edit
-	end
-
-	def create
+		@post = Post.find(params[:id])
 	end
 
 	def show
+		@post = Post.find(params[:id])
 	end
 
 	def update
+		@post = Post.find(params[:id])
+		if @post.update_attributes(params[:post])
+			flash[:notice] = "Post saved."
+			redirect_to post_url(@post)
+		else
+			render :action => :edit
+		end
 	end
 
 	def delete
