ea23381f0908b0bd9a41dfc3ffd25873ad6c673a
Tyler Cunnion <tyler.cunnion@gmail.com>
2010-02-25 00:20:07 -0500

    Cleaned up post display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,12 @@ module ApplicationHelper
     }.merge(options)
     return '<time datetime="' + time.strftime(options[:attribute_format]) + '">' + time.strftime(options[:text_format]) + '</time>'
   end
+  
+  def smart_header(content, level=1, attributes={})
+    h_string = '<h' + level.to_s
+    attributes.each {|key, value| h_string = h_string + ' ' + key + '="' + value + '"' }
+    h_string = h_string + '>' + content + '</h' + level.to_s + '>'
+    
+    return h_string
+  end
 end
