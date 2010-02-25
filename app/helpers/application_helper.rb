d08f9770acb1d9191e6f42cbba725fdc8a925ea2
Tyler Cunnion <tyler.cunnion@gmail.com>
2010-02-24 23:49:18 -0500

    Formatting on post page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def time_element(time, options={})
+    options = {
+      # YYYY-MM-DDThh:mmTZD
+      :attribute_format => '%Y-%m-%dT%H:%M%z',
+      :text_format => '%B %e, %Y'
+    }.merge(options)
+    return '<time datetime="' + time.strftime(options[:attribute_format]) + '">' + time.strftime(options[:text_format]) + '</time>'
+  end
 end
