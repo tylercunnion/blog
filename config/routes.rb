0d5312594e1cde0c3270061213279e62ed4095be
Tyler Cunnion <tyler.cunnion@gmail.com>
2010-02-24 22:01:39 -0500

    Added default route to posts list

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => "users"
   map.resources :users
   map.resources :posts
+  
+  map.root :posts
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
