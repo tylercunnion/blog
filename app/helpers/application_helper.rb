fe4f6f219359002442c6a017c7996a79e7dfb0db
Tyler Cunnion <tyler.cunnion@gmail.com>
2010-02-25 16:22:47 -0500

    Following HTML5 spec re:headers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,12 +9,4 @@ module ApplicationHelper
     }.merge(options)
     return '<time datetime="' + time.utc.strftime(options[:attribute_format]) + '">' + time.localtime.strftime(options[:text_format]) + '</time>'
   end
-  
-  def smart_header(content, level=1, html_options={})
-    h_string = '<h' + level.to_s
-    html_options.each {|key, value| h_string = h_string + ' ' + key + '="' + value + '"' }
-    h_string = h_string + '>' + content + '</h' + level.to_s + '>'
-    
-    return h_string
-  end
 end
