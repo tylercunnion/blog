f37bf289b722e56bcaa401953822d3fef07e0750
Tyler Cunnion <tyler.cunnion@gmail.com>
2010-02-26 18:53:18 -0500

    Rejigger post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 	belongs_to :author, :class_name => "User", :counter_cache => true
 	
-	before_validation :create_slug, :create_summary
+	before_validation :create_slug
 	
 	validates_presence_of :title, :text
 	validates_uniqueness_of :slug
@@ -10,10 +10,5 @@ class Post < ActiveRecord::Base
 	private
 	  def create_slug
 	    self.slug ||= self.title.gsub(" ", "_")
-    end
-    
-    # TODO: Make a clever algorithm here
-    def create_summary
-      self.summary = self.text
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,12 +9,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100226205907) do
+ActiveRecord::Schema.define(:version => 20100226232935) do
 
   create_table "posts", :force => true do |t|
     t.string   "title",                         :null => false
     t.string   "slug",                          :null => false
-    t.string   "summary"
+    t.text     "summary"
     t.text     "text",                          :null => false
     t.boolean  "published",  :default => false
     t.datetime "created_at"
