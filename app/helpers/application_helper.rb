66a6cd626bdf974d07b7b4d05fca5865fddb39e3
Tyler Cunnion <tyler.cunnion@gmail.com>
2010-02-26 15:50:57 -0500

    Twitter changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   
-  def time_element(time, options={})
+  def time_element(time, options={}, html_attributes={})
     options = {
       # YYYY-MM-DDThh:mmTZD
       :attribute_format => '%Y-%m-%dT%H:%MZ',
       :text_format => '%B %e, %Y'
     }.merge(options)
-    return '<time datetime="' + time.utc.strftime(options[:attribute_format]) + '">' + time.localtime.strftime(options[:text_format]) + '</time>'
+    element_text = '<time datetime="' + time.utc.strftime(options[:attribute_format]) + '"'
+    html_attributes.each do |key, value|
+      element_text = element_text + " #{key}=\"#{value}\""
+    end      
+    return element_text + '>' + time.localtime.strftime(options[:text_format]) + '</time>'
   end
 end
