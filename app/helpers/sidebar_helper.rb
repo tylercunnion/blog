44d46de3f943489250f66047b8c9aa826ebfb701
Tyler Cunnion <tyler.cunnion@gmail.com>
2010-03-01 10:49:52 -0500

    Regular expressions for linkifying tweets

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -1,9 +1,9 @@
 module SidebarHelper
   
   def linkify_twitter(text)
-    text = text.gsub(/@(\w*)/, '<a href="http://twitter.com/\1">@\1</a>')
-    text = text.gsub(/#(\w*)/, '<a href="http://twitter.com/search?q=%23\1">#\1</a>')
-    
+    text = text.gsub(/(\s|\A)@(\w*)/, '\1<a href="http://twitter.com/\2">@\2</a>')
+    text = text.gsub(/(\s|\A)#(\w*)/, '\1<a href="http://twitter.com/search?q=%23\2">#\2</a>')
+    text = text.gsub(/(\s|\A)(https?:\/\/[\S]*)/, '\1<a href="\2">\2</a>')
     return text
   end
 end
